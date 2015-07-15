a3e2f5678dd5f009a2232598116905cd8ee99352
= <jonathanbeaudoin1996@hotmail.com>
2015-07-15 04:35:32 -0400

    Whoops!

--- src/org/jetbrains/java/decompiler/modules/decompiler/exps/AssignmentExprent.java
+++ src/org/jetbrains/java/decompiler/modules/decompiler/exps/AssignmentExprent.java
@@ -32,186 +32,177 @@ import java.util.Set;
 
 public class AssignmentExprent extends Exprent {
 
-	public static final int CONDITION_NONE = -1;
+  public static final int CONDITION_NONE = -1;
 
-	private static final String[] OPERATORS = {
-			" += ",   // FUNCTION_ADD
-			" -= ",   // FUNCTION_SUB
-			" *= ",   // FUNCTION_MUL
-			" /= ",   // FUNCTION_DIV
-			" &= ",   // FUNCTION_AND
-			" |= ",   // FUNCTION_OR
-			" ^= ",   // FUNCTION_XOR
-			" %= ",   // FUNCTION_REM
-			" <<= ",  // FUNCTION_SHL
-			" >>= ",  // FUNCTION_SHR
-			" >>>= "  // FUNCTION_USHR
-	};
+  private static final String[] OPERATORS = {
+    " += ",   // FUNCTION_ADD
+    " -= ",   // FUNCTION_SUB
+    " *= ",   // FUNCTION_MUL
+    " /= ",   // FUNCTION_DIV
+    " &= ",   // FUNCTION_AND
+    " |= ",   // FUNCTION_OR
+    " ^= ",   // FUNCTION_XOR
+    " %= ",   // FUNCTION_REM
+    " <<= ",  // FUNCTION_SHL
+    " >>= ",  // FUNCTION_SHR
+    " >>>= "  // FUNCTION_USHR
+  };
 
-	private Exprent left;
-	private Exprent right;
-	private int condType = CONDITION_NONE;
+  private Exprent left;
+  private Exprent right;
+  private int condType = CONDITION_NONE;
 
-	public AssignmentExprent(Exprent left, Exprent right, Set<Integer> bytecodeOffsets) {
-		super(EXPRENT_ASSIGNMENT);
-		this.left = left;
-		this.right = right;
+  public AssignmentExprent(Exprent left, Exprent right, Set<Integer> bytecodeOffsets) {
+    super(EXPRENT_ASSIGNMENT);
+    this.left = left;
+    this.right = right;
 
-		addBytecodeOffsets(bytecodeOffsets);
-	}
+    addBytecodeOffsets(bytecodeOffsets);
+  }
 
-	@Override
-	public VarType getExprType() {
-		return left.getExprType();
-	}
+  @Override
+  public VarType getExprType() {
+    return left.getExprType();
+  }
 
-	@Override
-	public CheckTypesResult checkExprTypeBounds() {
-		CheckTypesResult result = new CheckTypesResult();
+  @Override
+  public CheckTypesResult checkExprTypeBounds() {
+    CheckTypesResult result = new CheckTypesResult();
 
-		VarType typeLeft = left.getExprType();
-		VarType typeRight = right.getExprType();
+    VarType typeLeft = left.getExprType();
+    VarType typeRight = right.getExprType();
 
-		if (typeLeft.typeFamily > typeRight.typeFamily) {
-			result.addMinTypeExprent(right, VarType.getMinTypeInFamily(typeLeft.typeFamily));
-		} else if (typeLeft.typeFamily < typeRight.typeFamily) {
-			result.addMinTypeExprent(left, typeRight);
-		} else {
-			result.addMinTypeExprent(left, VarType.getCommonSupertype(typeLeft, typeRight));
-		}
+    if (typeLeft.typeFamily > typeRight.typeFamily) {
+      result.addMinTypeExprent(right, VarType.getMinTypeInFamily(typeLeft.typeFamily));
+    }
+    else if (typeLeft.typeFamily < typeRight.typeFamily) {
+      result.addMinTypeExprent(left, typeRight);
+    }
+    else {
+      result.addMinTypeExprent(left, VarType.getCommonSupertype(typeLeft, typeRight));
+    }
 
-		return result;
-	}
+    return result;
+  }
 
-	@Override
-	public List<Exprent> getAllExprents() {
-		List<Exprent> lst = new ArrayList<Exprent>();
-		lst.add(left);
-		lst.add(right);
-		return lst;
-	}
+  @Override
+  public List<Exprent> getAllExprents() {
+    List<Exprent> lst = new ArrayList<Exprent>();
+    lst.add(left);
+    lst.add(right);
+    return lst;
+  }
 
-	@Override
-	public Exprent copy() {
-		return new AssignmentExprent(left.copy(), right.copy(), bytecode);
-	}
+  @Override
+  public Exprent copy() {
+    return new AssignmentExprent(left.copy(), right.copy(), bytecode);
+  }
 
-	@Override
-	public int getPrecedence() {
-		return 13;
-	}
+  @Override
+  public int getPrecedence() {
+    return 13;
+  }
 
-	@Override
-	public TextBuffer toJava(int indent, BytecodeMappingTracer tracer) {
-		VarType leftType = left.getExprType();
-		VarType rightType = right.getExprType();
+  @Override
+  public TextBuffer toJava(int indent, BytecodeMappingTracer tracer) {
+    VarType leftType = left.getExprType();
+    VarType rightType = right.getExprType();
 
-		boolean fieldInClassInit = false, hiddenField = false;
-		if (left.type == Exprent.EXPRENT_FIELD) { // first assignment to a final field. Field name without "this" in front of it
-			FieldExprent field = (FieldExprent) left;
-			ClassNode node = ((ClassNode) DecompilerContext.getProperty(DecompilerContext.CURRENT_CLASS_NODE));
-			if (node != null) {
-				StructField fd = node.classStruct.getField(field.getName(), field.getDescriptor().descriptorString);
-				if (fd != null) {
-					if (field.isStatic() && fd.hasModifier(CodeConstants.ACC_FINAL)) {
-						fieldInClassInit = true;
-					}
-					if (node.getWrapper() != null && node.getWrapper().getHiddenMembers().contains(InterpreterUtil.makeUniqueKey(fd.getName(), fd.getDescriptor()))) {
-						hiddenField = true;
-					}
-				}
-			}
-		}
-
-		if (hiddenField) {
-			return new TextBuffer();
-		}
-
-		TextBuffer buffer = new TextBuffer();
-
-		if (fieldInClassInit) {
-			buffer.append(((FieldExprent) left).getName());
-		} else {
-			buffer.append(left.toJava(indent, tracer));
-		}
-
-		TextBuffer res = right.toJava(indent, tracer);
-
-		if (condType == CONDITION_NONE &&
-				!leftType.isSuperset(rightType) &&
-				(rightType.equals(VarType.VARTYPE_OBJECT) || leftType.type != CodeConstants.TYPE_OBJECT)) {
-			if (right.getPrecedence() >= FunctionExprent.getPrecedence(FunctionExprent.FUNCTION_CAST)) {
-				res.enclose("(", ")");
-			}
-
-			res.prepend("(" + ExprProcessor.getCastTypeName(leftType) + ")");
-		}
-
-		buffer.append(condType == CONDITION_NONE ? " = " : OPERATORS[condType]).append(res);
-
-		tracer.addMapping(bytecode);
-
-		return buffer;
-	}
-
-	@Override
-	public boolean replaceExprent(Exprent oldExpr, Exprent newExpr) {
-		boolean b;
-		if (oldExpr == left) {
-			left = newExpr;
-			b = true;
-		} else {
-			b = false;
-		}
-		if (oldExpr == right) {
-			right = newExpr;
-			b = true;
-		} else {
-			b = false;
-		}
-		return b;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-      if (o == this) {
-        return true;
+    boolean fieldInClassInit = false, hiddenField = false;
+    if (left.type == Exprent.EXPRENT_FIELD) { // first assignment to a final field. Field name without "this" in front of it
+      FieldExprent field = (FieldExprent)left;
+      ClassNode node = ((ClassNode)DecompilerContext.getProperty(DecompilerContext.CURRENT_CLASS_NODE));
+      if (node != null) {
+        StructField fd = node.classStruct.getField(field.getName(), field.getDescriptor().descriptorString);
+        if (fd != null) {
+          if (field.isStatic() && fd.hasModifier(CodeConstants.ACC_FINAL)) {
+            fieldInClassInit = true;
+          }
+          if (node.getWrapper() != null && node.getWrapper().getHiddenMembers().contains(InterpreterUtil.makeUniqueKey(fd.getName(), fd.getDescriptor()))) {
+            hiddenField = true;
+          }
+        }
       }
-      if (o == null || !(o instanceof AssignmentExprent)) {
-        return false;
+    }
+
+    if (hiddenField) {
+      return new TextBuffer();
+    }
+
+    TextBuffer buffer = new TextBuffer();
+
+    if (fieldInClassInit) {
+      buffer.append(((FieldExprent)left).getName());
+    }
+    else {
+      buffer.append(left.toJava(indent, tracer));
+    }
+
+    TextBuffer res = right.toJava(indent, tracer);
+
+    if (condType == CONDITION_NONE &&
+        !leftType.isSuperset(rightType) &&
+        (rightType.equals(VarType.VARTYPE_OBJECT) || leftType.type != CodeConstants.TYPE_OBJECT)) {
+      if (right.getPrecedence() >= FunctionExprent.getPrecedence(FunctionExprent.FUNCTION_CAST)) {
+        res.enclose("(", ")");
       }
 
-		AssignmentExprent as = (AssignmentExprent) o;
-		return InterpreterUtil.equalObjects(left, as.getLeft()) &&
-				InterpreterUtil.equalObjects(right, as.getRight()) &&
-				condType == as.getCondType();
-	}
+      res.prepend("(" + ExprProcessor.getCastTypeName(leftType) + ")");
+    }
 
-	// *****************************************************************************
-	// getter and setter methods
-	// *****************************************************************************
+    buffer.append(condType == CONDITION_NONE ? " = " : OPERATORS[condType]).append(res);
 
-	public Exprent getLeft() {
-		return left;
-	}
+    tracer.addMapping(bytecode);
 
-	public void setLeft(Exprent left) {
-		this.left = left;
-	}
+    return buffer;
+  }
 
-	public Exprent getRight() {
-		return right;
-	}
+  @Override
+  public void replaceExprent(Exprent oldExpr, Exprent newExpr) {
+    if (oldExpr == left) {
+      left = newExpr;
+    }
+    if (oldExpr == right) {
+      right = newExpr;
+    }
+  }
 
-	public void setRight(Exprent right) {
-		this.right = right;
-	}
+  @Override
+  public boolean equals(Object o) {
+    if (o == this) return true;
+    if (o == null || !(o instanceof AssignmentExprent)) return false;
 
-	public int getCondType() {
-		return condType;
-	}
+    AssignmentExprent as = (AssignmentExprent)o;
+    return InterpreterUtil.equalObjects(left, as.getLeft()) &&
+           InterpreterUtil.equalObjects(right, as.getRight()) &&
+           condType == as.getCondType();
+  }
 
-	public void setCondType(int condType) {
-		this.condType = condType;
-	}
+  // *****************************************************************************
+  // getter and setter methods
+  // *****************************************************************************
+
+  public Exprent getLeft() {
+    return left;
+  }
+
+  public void setLeft(Exprent left) {
+    this.left = left;
+  }
+
+  public Exprent getRight() {
+    return right;
+  }
+
+  public void setRight(Exprent right) {
+    this.right = right;
+  }
+
+  public int getCondType() {
+    return condType;
+  }
+
+  public void setCondType(int condType) {
+    this.condType = condType;
+  }
 }
